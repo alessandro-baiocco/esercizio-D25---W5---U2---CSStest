ec9d19cfc21eb777133d86e4cb142d2eb801e5a1
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-07-29 14:15:43 +0200

    more animated

--- animation.js
+++ animation.js
@@ -1,9 +1,16 @@
 const navBar = document.getElementsByTagName("nav")[0];
+const button = document.querySelector(".navList ul button ");
 
 window.addEventListener("scroll", function () {
   if (this.scrollY > 400) {
-    navBar.classList.add("white");
+    navBar.classList.remove("toYellow");
+    button.classList.remove("toBlack");
+    navBar.classList.add("toWhite");
+    button.classList.add("toGreen");
   } else {
-    navBar.classList.remove("white");
+    navBar.classList.remove("toWhite");
+    button.classList.remove("toGreen");
+    navBar.classList.add("toYellow");
+    button.classList.add("toBlack");
   }
 });
